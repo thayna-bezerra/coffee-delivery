f05713d8713564dae84d023b8b85a9f1820c97a6
thayna <arraisthayna2106@gmail.com>
2023-05-15 16:14:59 -0300

    creating route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/theme/default";
+import { Router } from './Router';
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <p>
-        Hello World
-      </p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
