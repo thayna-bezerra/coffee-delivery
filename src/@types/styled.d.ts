d8cee097e5c116e2804396bb78c69f7e14d5819e
thayna <arraisthayna2106@gmail.com>
2023-05-15 16:06:33 -0300

    add global styling

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import { defaultTheme } from "../styles/theme/default";
+
+type ThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {
+    _brand?: never;
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
+
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <p>
         Hello World
       </p>
-    </>
+    </ThemeProvider>
   )
 }
