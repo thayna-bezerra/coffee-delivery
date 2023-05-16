7a6afa6051c8d81531bc30ddfc1cce8fd200020c
thayna <arraisthayna2106@gmail.com>
2023-05-16 16:37:47 -0300

    completing routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { Route, Routes} from "react-router-dom";
 import { HomePage } from "./pages/Home";
+import { DefaultLayout } from "./layouts/DefaultLayout";
+import { CompleteOrderPage } from "./CompleteOrder";
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<DefaultLayout/>}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/completeOrder" element={<CompleteOrderPage />} />
+      </Route>
     </Routes>
   )
 }
