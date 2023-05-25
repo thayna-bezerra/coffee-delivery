65b202c7e6dcc022b24d5b05119657a9b2e4b656
thayna <arraisthayna2106@gmail.com>
2023-05-25 16:32:18 -0300

    add logo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import { HeaderContainer } from "./styles";
+import coffeeLogoImg from '../../assets/coffee-delivery-logo.svg'
+
+export function Header() {
+  return {
+    <HeaderContainer>
+      <div>
+        <img src="coffeeLogoImg" alt="" />
+      </div>
+    </HeaderContainer>s
+  }
+}
