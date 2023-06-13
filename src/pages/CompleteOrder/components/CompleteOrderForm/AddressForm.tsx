f227bf4bad4e435937451d0de5df05239d56cd2e
thayna <arraisthayna2106@gmail.com>
2023-06-13 20:51:27 -0300

    finalized grid on address card

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -4,7 +4,13 @@ import { AddressFormContainer } from "./styles";
 export function AddressForm() {
   return (
     <AddressFormContainer>
-      <Input />
+      <Input placeholder="CEP" type="number" className="cep" />
+      <Input placeholder="Rua" className="street" />
+      <Input placeholder="Número" type="number" />
+      <Input placeholder="Complemento" className="complement" />
+      <Input placeholder="Bairro" />
+      <Input placeholder="Cidade" />
+      <Input placeholder="UF" />
     </AddressFormContainer>
   )
 }
