c148826555882ccbb78a8d6342be1f2c625e9bd2
thayna <arraisthayna2106@gmail.com>
2023-06-16 22:48:58 -0300

    saving cart items to local storage

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useEffect, useState } from "react";
 import { Coffee } from "../pages/Home/components/CoffeeCard";
 import { produce } from "immer";
 
@@ -19,10 +19,18 @@ interface CartContextProviderProps {
   children: ReactNode;
 }
 
+const COFFEE_ITEMS_STORAGE_KEY = "coffeeDelivery:cartItems"
+
 export const CartContext = createContext({} as CartContextType);
 
 export function CartContextProvider({ children }: CartContextProviderProps) {
-  const[cartItems, setCartItems] = useState<CartItem[]>([]);
+  const[cartItems, setCartItems] = useState<CartItem[]>(() => {
+    const storedCartItems = localStorage.getItem(COFFEE_ITEMS_STORAGE_KEY)
+    if (storedCartItems) {
+      return JSON.parse(storedCartItems);
+    }
+    return [];
+  });
 
   const cartQuantity = cartItems.length;
 
@@ -78,6 +86,10 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
 
     setCartItems(newCart);
   }
+
+  useEffect(() => {
+    localStorage.setItem(COFFEE_ITEMS_STORAGE_KEY, JSON.stringify(cartItems));
+  }, [cartItems])
   
   return (
     <CartContext.Provider value={{ 
